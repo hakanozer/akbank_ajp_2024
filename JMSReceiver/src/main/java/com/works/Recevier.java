91dbe8a0c8f0cd22b3ba76e5f46012bffa09e79e
Hakan ÖZER <hakanozer02@gmail.com>
2024-03-22 10:36:35 +0300

    JMS

---
+++ JMSReceiver/src/main/java/com/works/Recevier.java
@@ -0,0 +1,15 @@
+package com.works;
+
+import com.works.models.Email;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Recevier {
+
+    @JmsListener(destination = "mailDestination", containerFactory = "mailContainerFactory")
+    public void recevierEmail(Email email) {
+        System.out.println("Send Email:" + email);
+    }
+
+}
